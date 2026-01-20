4c779b412bf4ea4082438d2e63c6c3d032781b00
MauriceZ48 <satampsatamp46@gmail.com>
2026-01-20 17:17:14 +0700

    update seeder and factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ApplicationSeeder::class,
+            AttachmentSeeder::class,
         ]);
     }
 }
